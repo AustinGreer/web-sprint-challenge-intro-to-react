ba9dab6389da52c2cea680d13fc0a51ca1b8efeb
Austin Greer <agreer.ag12@gmail.com>
2021-02-19 17:46:42 -0500

    mapped over data where character component was added. Added styling as well. mvp met.

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
-import React, { useState, useEffect}
- from 'react';
- import axios from 'axios';
+import React, { useState, useEffect} from 'react';
+import axios from 'axios';
+import Character from './components/Character'
 import './App.css';
 
 const App = () => {
   // Try to think through what state you'll need for this app before starting. Then build out
   // the state properties here.
-  const [starWarsData, setStarWarsData] = useState([])
+  const [starWarsCharacters, setStarWarsCharacters] = useState([])
 
   // Fetch characters from the API in an effect hook. Remember, anytime you have a 
   // side effect in a component, you want to think about which state and/or props it should
@@ -15,17 +15,26 @@ const App = () => {
     axios.get('https://swapi.dev/api/people/')
     .then(res => {
       console.log(res.data)
-      setStarWarsData(res.data)
+      setStarWarsCharacters(res.data)
     })
     .catch(err => {
       console.log(err)
     })
   }, [])
-
+  
   return (
     <div className="App">
       <h1 className="Header">Characters</h1>
-      
+      {
+        starWarsCharacters.map(character => {
+          return (
+            <Character
+              key={character.created}
+              character={character}
+            />
+          )
+        })
+      }
     </div>
   );
 }
--- src/components/Character.js
+++ src/components/Character.js
@@ -1,1 +1,44 @@
 // Write your Character component here
+import React from 'react'
+import styled from 'styled-components'
+
+
+const StyledDiv = styled.div `
+    display: flex;
+    justify-content: center;
+`
+
+const StyledContainer = styled.div `
+    color: white;
+    background: black;
+    width: 30%;
+    margin-bottom: 5%;
+    display: flex;
+    justify-content: space-evenly;
+`
+const StyledParagraph = styled.p `
+    font-family: sans-serif;
+    font-size: 1.5rem;
+`
+
+
+
+const Character = (props) => {
+    // This component will receive the characters list from state
+    // and render individual character cards.
+    // For now, this component will render simply the name and birthdate.
+    // If additional time allows after meeting mvp, we will add more data and create a drop down toggle for each card.
+    const {character} = props
+    console.log(character)
+    return (
+        <StyledDiv>
+            <StyledContainer>
+                <StyledParagraph>{character.name}</StyledParagraph>
+                <StyledParagraph>{character.birth_year}</StyledParagraph>
+            </StyledContainer>
+
+        </StyledDiv>
+    );
+}
+
+export default Character;
